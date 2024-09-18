8773843303626e526ad0a90e74d5131255595751
RobertsEihe <roberts.eihe@gmail.com>
2024-09-18 11:23:42 +0300

    Agent code fixes

--- src/main/java/com/project/InsuranceProject/views/shared/LoginView.java
+++ src/main/java/com/project/InsuranceProject/views/shared/LoginView.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterListener;
@@ -16,7 +17,7 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 @Route(value = "login", layout = AppLayoutNavbar.class)
 @PageTitle("LoginView")
 @AnonymousAllowed
-//@CssImport("./styles/shared-styles.css")
+@CssImport("./styles/shared-styles.css")
 public class LoginView extends VerticalLayout implements BeforeEnterListener {
 
     private LoginForm login = new LoginForm();
@@ -29,11 +30,13 @@ public class LoginView extends VerticalLayout implements BeforeEnterListener {
 
         login.setAction("login");
 
-        Anchor registerLink = new Anchor("register", "Register");
+
+
+        //Anchor registerLink = new Anchor("register", "Sign up");
         add(
                 new H1("Insurance System"),
-                login,
-                registerLink
+                login
+                //registerLink
         );
 
     }
@@ -46,5 +49,16 @@ public class LoginView extends VerticalLayout implements BeforeEnterListener {
                 .containsKey("error")){
             login.setError(true);
         }
+    }
+
+    public void LoginValidation() {
+        // Use LoginOverlay if you want a more advanced login popup (optional)
+        LoginOverlay loginOverlay = new LoginOverlay();
+        loginOverlay.setError(true);
+        add(loginOverlay);
+        loginOverlay.setOpened(true);
+
+        // Prevent the example from stealing focus when browsing the documentation
+        loginOverlay.getElement().setAttribute("no-autofocus", "");
     }
 }
