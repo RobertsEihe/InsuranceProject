e6f1e2c5fcd433c35ccd75eaa9df8946da9feb5e
RobertsEihe <roberts.eihe@gmail.com>
2024-09-18 09:37:06 +0300

    make images public access

--- src/main/java/com/project/InsuranceProject/security/SecurityConfig.java
+++ src/main/java/com/project/InsuranceProject/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,6 +35,12 @@ public class SecurityConfig extends VaadinWebSecurity {
                                 .loginPage("/login")
                                 .successHandler(successHandler)
                                 .permitAll());
+    }
+
+    @Override
+    protected void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers("/images/**");
+        super.configure(web);
     }
 
     @Bean
